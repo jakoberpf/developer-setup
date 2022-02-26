38db62ea14e6b35d9c1ad976c51471fd03682bcd
Jakob Erpf <58260820+jakoberpf@users.noreply.github.com>
2022-02-26 13:21:25 +0100

    update setup yaml

--- setup.sh
+++ setup.sh
@@ -60,6 +60,21 @@ then
   abort "[error] The developer-setup is only supported on macOS and Linux."
 fi
 
+# Check the environment variables
+if [ -n "$OPS_GITHUB_USERNAME" ]; then
+  GITHUB_USERNAME=$OPS_GITHUB_USERNAME
+elif [ ! -n "$GITHUB_USERNAME" ]; then
+  echo "Enter your Github Username: "
+  read GITHUB_USERNAME
+fi
+
+if [ -n "$OPS_GGITHUB_TOKEN" ]; then
+  GITHUB_TOKEN=$OPS_GITHUB_TOKEN
+elif [ ! -n "$GITHUB_TOKEN" ]; then
+  echo "Enter Your Github Token: "
+  read GITHUB_TOKEN
+fi
+
 # GITHUB_TOKEN=""
 GITHUB_REPO_SETUP="my-setup"
 GITHUB_REPO_HOME="my-home"
@@ -100,11 +115,8 @@ then
     echo "[setup] AppleSilicon detected, will install Rosetta."
     sudo softwareupdate --install-rosetta --agree-to-license
   fi
-  ~/$LOCALREPO_SETUP/setup.mac.sh
+  ~/$LOCALREPO_HOME/bin/config.sh ~/$LOCALREPO_SETUP
 elif [[ "${OS}" == "Linux" ]]
 then
   echo "[setup] Running setup for Ubuntu/Linux."
 fi
-
-echo "[custom] Run the custom configuration"
-~/$LOCALREPO_HOME/bin/run.sh
